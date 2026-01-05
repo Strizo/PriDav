2e7ff9f2502238cecf55398f12154db907faf46b
PrutkayFilip <prutkay.filip@gmail.com>
2026-01-05 14:05:27 +0100

    Update stanza.py

--- stanza.py
+++ stanza.py
@@ -2,7 +2,6 @@ stanza.download("sk")
 nlp = stanza.Pipeline("sk", processors="tokenize,pos,lemma")
 
 def lemmatize(text: str) -> str:
-    """Lemmatizácia + odstránenie stop-slov."""
     doc = nlp(text)
     lemmas = []
     for sent in doc.sentences:
